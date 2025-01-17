308a5c7163b608787730cc8e33ef11f3d4f0cb3f
Túlius <tulius_sr@outlook.com>
2025-01-17 11:13:01 -0300

    Adicionando cards dinâmicos

--- cinema/src/pages/inicio/index.js
+++ cinema/src/pages/inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import pictures from 'json/db.json';
+import styles from './Inicio.module.css';
 
 function Inicio() {
     return (
@@ -12,7 +14,11 @@ function Inicio() {
             <Titulo>
                 <h1>Filmes em cartaz!</h1>
             </Titulo>
-            <Card id='1' titulo='Ainda estou aqui' capa='https://br.web.img3.acsta.net/r_1920_1080/img/b1/35/b1351babe7a777b7a97eded3db356990.jpg'/>
+            <section className={styles.container}>
+                {pictures.map((picture) =>{
+                    return <Card {...picture} key={picture.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
