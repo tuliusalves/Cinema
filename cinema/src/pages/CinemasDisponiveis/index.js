e8dec23128d0f490a513c67a18196cbac8e1e8dc
Túlius <tulius_sr@outlook.com>
2025-02-22 13:31:44 -0300

    Editando  pagina de cinemasDisponiveis

--- cinema/src/pages/CinemasDisponiveis/index.js
+++ cinema/src/pages/CinemasDisponiveis/index.js
@@ -1,34 +1,101 @@
 import styled from "styled-components";
-import Card from "components/Cards/Card";
 import Header from "components/Header";
-import Rodape from "components/Rodape";
-import MainDiv from "components/MainDiv";
 import React from "react";
-
+import Recife from "../../assets/Shopping Recife.jpg";
+import Tacaruna from "../../assets/Shopping Tacaruna.jpg";
+import Plaza from "../../assets/Shopping plaza.jpg";
+import Rodape from "components/Rodape";
 
 const AppContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: #313331;
-  padding-bottom: 100px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  color: #ffff;
-`;
+    width:100%;
+    background-color: #313331;
+    display: flex;
+    justify-content: center;
+    flex-direction:column;
+    align-items:center;
+    color:#FFFF;
+  
+`
 const AppContainerMain = styled.div`
-  margin-top: 50px;
-`;
+     width: 100%;
+     margin-top: 65px;
+     display: flex;
+     flex-direction: row;
+     align-items: center;
+`
+const MainContainer = styled.div`
+     width: 90%;
+     background-color: #1e1e1e;
+     border-radius: 25px;
+     display: flex;
+     flex-direction: column;
+     justify-content: space-between;
+     align-items: center;
+     margin: auto;
+`
+const TituloPagina= styled.h3`
+     font-size:26px;
+     margin-top:68px;
+`
+const ShoppingContainer = styled.div`
+    width:90%;
+    background-size:cover;
+    border-radius:25px;
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 50px auto 90px auto;
+`
+//Colocar alinhados em linha
+const ShoppingCard = styled.div`
+    width:271px;
+    height:230px;
+    background-size:cover;
+    align-items:center;
+    border-radius:25px;
+    border: 1px solid white;
+    margin: 30px auto 30px auto;
+    display:flex;
+    flex-direction:column;
+`
+const ShoppingImg = styled.img`
+    width:90%;
+    background-size:cover;
+    background-position:center;
+    margin: 20px auto auto auto;    
+`
+const ShoppingTitulo= styled.h3`
+    font-size:16px;
+    margin-top:28px;
+`
 
 function CinemasDisponiveis() {
   return (
-    <AppContainer>
-        <Header />
-        <AppContainerMain>
-          
-        </AppContainerMain>
-      </AppContainer>
+    <div>
+       <AppContainer>
+            <Header/>
+            <AppContainerMain>
+             <MainContainer>
+              <TituloPagina>CinemasDisponiveis</TituloPagina>
+              <ShoppingContainer>
+                <ShoppingCard>
+                  <ShoppingTitulo>Shopping Tacaruna</ShoppingTitulo>
+                  <ShoppingImg src={Tacaruna} alt="Tacaruna"></ShoppingImg>
+                </ShoppingCard>
+                <ShoppingCard>
+                  <ShoppingTitulo>Shopping Recife</ShoppingTitulo>
+                  <ShoppingImg src={Recife} alt="Recife"></ShoppingImg>
+                </ShoppingCard>
+                <ShoppingCard>
+                  <ShoppingTitulo>Shopping Plaza</ShoppingTitulo>
+                  <ShoppingImg src={Plaza} alt="Plaza"></ShoppingImg>
+                </ShoppingCard>
+               </ShoppingContainer> 
+              <Rodape/>
+             </MainContainer>
+            </AppContainerMain> 
+        </AppContainer>
+    </div>
   );
 }
 
